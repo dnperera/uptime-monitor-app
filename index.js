90598c1c1c0a3b85935a09c57788e7896d904c6e
Dayan Perera <dayannp@gmail.com>
2018-07-06 13:12:32 -0700

    add create token method and route, also random token generator helper

--- index.js
+++ index.js
@@ -96,5 +96,6 @@ var unifiedServer = (req, res) => {
 //Define Request Router
 var router = {
   ping: handlers.ping,
-  users: handlers.users
+  users: handlers.users,
+  tokens: handlers.tokens
 };
